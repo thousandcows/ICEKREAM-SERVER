88058f5461bc37a98c7ce807af28898e9bc9fbe5
thousandcows <milcamboo@gmail.com>
2022-11-29 22:51:34 +0900

    feat: project initialization

---
+++ src/main/java/ICEKREAM/ICEKREAM/IcekreamApplication.java
@@ -0,0 +1,13 @@
+package ICEKREAM.ICEKREAM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcekreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcekreamApplication.class, args);
+	}
+
+}
